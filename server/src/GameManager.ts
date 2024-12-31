56592d99b2eaf201ef2588582099d3a8b67568b8
abhishek <abhishek4code@gmail.com>
2024-12-31 15:40:53 +0530

    feat: users and game structure created

---
+++ server/src/GameManager.ts
@@ -0,0 +1,51 @@
+import { WebSocket } from "ws";
+import { INIT_MSG } from "./Message";
+import { Game } from "./Game";
+// interface Game{
+//     player1: WebSocket,
+//     player2: WebSocket,
+//     moves: String[],
+// }
+
+export class GameManager{
+    private games: Game[];
+    private users: WebSocket[];
+    private pendinguser: WebSocket |  null;
+
+
+    constructor(){
+        this.games = [];
+        this.pendinguser = null;
+        this.users = []
+    }
+
+    adduser(socket:WebSocket) {
+        this.users.push(socket);
+    }
+    removeuser(socket:WebSocket){
+        this.users = this.users.filter((user) => user !== socket);
+        // Todo: you can write a reconnection logic later on
+    }
+
+    private addHandler(socket: WebSocket){
+        socket.on("message", (data)=> {
+            const message = JSON.parse(data.toString());
+            console.log("message", message);
+
+            if(message.type === INIT_MSG){
+                if(this.pendinguser){
+                    // so when two people get in the queue start the game
+                    const game = new Game(socket, this.pendinguser)
+                    //we will push this to our global game
+                    this.games.push(game);
+
+                    this.pendinguser = null;
+
+
+                }else {
+                    this.pendinguser = socket
+                }
+            }
+        })
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi there")
+import { WebSocketServer } from 'ws';
+import { GameManager } from './GameManager';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const gameManager = new GameManager();
+
+wss.on('connection', function connection(ws) {
+    
+  gameManager.adduser(ws);
+  wss.on("disconnect", () => gameManager.removeuser(ws))
+
+});
